9b807443a6d84dfb65775a4350ee5f7511008213
Lucas Marques <lucasmarquesds@gmail.com>
2015-04-28 22:12:47 -0300

    setting up ajax

--- coin_determiner/determiner/views.py
+++ coin_determiner/determiner/views.py
@@ -7,12 +7,15 @@ def index(request):
     coin = Coin()
     form = CoinForm()
     result = 0
+
     if request.method == 'POST':
         form = CoinForm(request.POST)
         if form.is_valid():
             result = coin.coinDeterminer(int(form['value'].value()))
+
     context = {
         'form': form,
         'result': result
     }
+
     return render(request, 'index.html', context)
